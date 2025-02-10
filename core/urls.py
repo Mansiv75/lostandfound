151b3865aae73d6791d7eef02579c5c0f64dfc9b
Mansiv75 <mv072004@gmail.com>
2025-02-10 22:21:48 +0530

    API documentation

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from .views import LostItemListCreateView, LostItemDeleteView, FoundItemListCreateView,  FoundItemDeleteView, MatchItemsView, UserRegisterView
 from .views import UserLoginView, UserLostItemView, UserFoundItemView, NearbyLostItemsView, NearbyFoundItemsView
 from rest_framework_simplejwt.views import TokenRefreshView
 from django.conf import settings
 from django.conf.urls.static import static
+from .swagger import schema_view
 
 urlpatterns = [
     path('users/register/', UserRegisterView.as_view(), name='register'),
@@ -14,9 +15,12 @@ urlpatterns = [
     path('found-items/', FoundItemListCreateView.as_view(), name='found-items'),
     path('found-items/<int:pk>/', FoundItemDeleteView.as_view(), name='found-item-delete'),
     path('match-items/', MatchItemsView.as_view(), name='match-items'),
-    path('lost-items/history', UserLostItemView.as_view(), name='lost-item-history'),
-    path('found-items/history', UserFoundItemView.as_view(), name='found-item-history'),
+    path('lost-items/history/', UserLostItemView.as_view(), name='lost-item-history'),
+    path('found-items/history/', UserFoundItemView.as_view(), name='found-item-history'),
     path('nearby-lost-items/', NearbyLostItemsView.as_view(), name='nearmost-lost-items'),
     path('nearby-found-items/', NearbyFoundItemsView.as_view(), name='nearmost-found-items'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    re_path(r"^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
 ]
 urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
