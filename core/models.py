cc010c6a91f92597db58621bf898cc9e3603c845
Mansiv75 <mv072004@gmail.com>
2025-02-10 12:40:49 +0530

    migrations applied

--- core/models.py
+++ core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class LostItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    location = models.CharField(max_length=100)
+    date = models.DateField()
+    reported_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Lost {self.name} o {self.date} at {self.location}"
+
+class FoundItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    location = models.CharField(max_length=100)
+    date = models.DateField()
+    reported_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Found {self.name} on {self.date} at {self.location}"
