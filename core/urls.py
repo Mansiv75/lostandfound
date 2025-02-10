d8e270e09604c71d4f6c55fc3adf9ae9f7db69d8
Mansiv75 <mv072004@gmail.com>
2025-02-10 13:25:49 +0530

    match items

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import LostItemCreateView, LostItemListView, LostItemDeleteView, FoundItemCreateView, FoundItemListView, FoundItemDeleteView
+from .views import LostItemCreateView, LostItemListView, LostItemDeleteView, FoundItemCreateView, FoundItemListView, FoundItemDeleteView, MatchItemsView
 
 urlpatterns = [
     path('lost-item/', LostItemCreateView.as_view(), name='lost-item-create'),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('found-item/', FoundItemCreateView.as_view(), name='found-item-create'),
     path('found-item/', FoundItemListView.as_view(), name='found-item-list'),
     path('found-item/<int:pk>/', FoundItemDeleteView.as_view(), name='found-item-delete'),
+    path('match-items/', MatchItemsView.as_view(), name='match-items'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from rest_framework import generics, filters
 from .models import LostItem, FoundItem
 from .serializers import UserSerializer, LostItemSerializer, FoundItemSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.db.models import Q
+
 
 #Report a lost item(POST)
 class LostItemCreateView(generics.CreateAPIView):
@@ -35,3 +39,24 @@ class FoundItemDeleteView(generics.DestroyAPIView):
     queryset = FoundItem.objects.all()
     serializer_class = FoundItemSerializer
 
+#Match Items
+
+class MatchItemsView(APIView):
+    def get(self, request):
+        matches=[]
+
+        lost_items = LostItem.objects.all()
+        found_items = FoundItem.objects.all()
+
+        for lost in lost_items:
+            for found in found_items:
+                if(
+                    lost.name.lower() == found.name.lower() and
+                    lost.location.lower() == found.location.lower() and
+                    (found.date-lost.date).days<=7
+                ):
+                    matches.append({
+                        'lost_item':LostItemSerializer(lost).data,
+                        'found_item':FoundItemSerializer(found).data,
+                    })
+        return Response(matches)
